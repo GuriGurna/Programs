8afe01bd322dee3fcb87a84c3bdf91b7063d13a0
guri <guri@gmail.com>
2023-05-27 14:51:33 +0530

    structure change

---
+++ pythonhardway/qr_code.py
@@ -0,0 +1,3 @@
+import qrcode 
+image = qrcode.make("https://youtu.be/eoOaKN4qCKw")
+image.save("fast.png ")
--- qr_code.py
+++
@@ -1,3 +0,0 @@
-import qrcode 
-image = qrcode.make("https://youtu.be/eoOaKN4qCKw")
-image.save("fast.png ")
